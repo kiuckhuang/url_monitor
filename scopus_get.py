4d9dbec85fa5edac602642ddae5666c1b75e93a8
Kiu Huang <ckhuang@life.hkbu.edu.hk>
2022-10-21 14:29:45 +0800

    added forloop

--- scopus_get.py
+++ scopus_get.py
@@ -4,10 +4,10 @@
 # pip install pytest-playwright
 # playwright install
 
-
+from time import sleep
+from random import randint
 from playwright.sync_api import sync_playwright
 
-url = 'https://www.scopus.com/authid/detail.uri?authorId=7006328373'
 
 playwright = sync_playwright().start()
 
@@ -18,21 +18,29 @@ context = browser.new_context(
     user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
 )
 page = browser.new_page()
-page.goto( url )
+
+url_prefix = 'https://www.scopus.com/authid/detail.uri?authorId='
+author_ids = ['14063470600', '9042311600', '6507012293']
+
+metrics = {
+    'name': '#scopus-author-profile-page-control-microui__general-information-content > section > div > h2',
+    'documents': '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(1) > h3',
+    'citations': '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(2) > h3',
+    'hindex': '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(3) > h3'
+}
 
 
-locator_documents = '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(1) > h3'
-locator_citations = '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(2) > h3'
-locator_hindex    = '#scopus-author-profile-page-control-microui__ScopusAuthorProfilePageControlMicroui > div:nth-child(2) > div > section > div > div.col-lg-6.col-24 > els-stack > div:nth-child(3) > h3'
+print(', '.join(metrics.keys()))
 
-page.wait_for_selector(locator_documents)
+for author_id in author_ids:
+    page.goto( url_prefix + author_id )
 
-docs = page.inner_text(locator_documents)
-cits = page.inner_text(locator_citations)
-hind = page.inner_text(locator_hindex)
-
-print(docs, cits, hind)
-
+    lst = [] # Declares an empty list named lst
+    for m_key, m_selector in metrics.items():
+        page.wait_for_selector(m_selector)
+        lst.append(page.inner_text(m_selector))
+    print(', '.join(lst))
+    sleep(randint(100,200)/100)
 browser.close()
 
 playwright.stop()
