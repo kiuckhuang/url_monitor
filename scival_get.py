ae7f30ea50bc8032f34320a303490c32df5bed2b
Kiu Huang <ckhuang@life.hkbu.edu.hk>
2022-10-27 14:58:26 +0800

    json_normalize

--- scival_get.py
+++ scival_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from distutils.log import error
 import requests
 import json
 import re
@@ -8,6 +9,8 @@ import os.path
 from os import path
 import math
 import pandas as pd
+from itertools import chain
+
 
 
 # max number of author ids
@@ -42,6 +45,11 @@ request_urls = {
     'Outputs_Top_Percentile_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=OutputsInTopCitationPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true'
 }
 
+# search urls
+request_urls = {
+    'Academic_Corporate_Collaboration_2017-2022':'https://kiu.cpeg.net/tmp/api/'
+}
+
 def main(argv):
     inputfile = ''
     outputfile = ''
@@ -69,7 +77,7 @@ def main(argv):
 
     lines = []
     with open(inputfile) as f:
-        lines = f.read().splitlines()[0:2]
+        lines = f.read().splitlines()
 
     for i in range(0, math.floor( len(lines) / max_per_request) + 1 ):
         index_start = i * max_per_request
@@ -80,12 +88,38 @@ def main(argv):
         url_paras['authors'] = ','.join(lines[index_start:index_end])
         p = requests.models.PreparedRequest()
         for myname, url_prefix in request_urls.items():
+            url_prefix = url_prefix + str(i) + '.json'
             p.prepare_url(url=url_prefix, params=url_paras)
             #print(p.url)
             json_dict = json.loads(requests.get(p.url, headers=request_headers).text)
-            #print(pd.json_normalize(json_dict['results'][0]['author']['name']))
-            print(pd.json_normalize(json_dict['results']))
+            df_all = pd.DataFrame(pd.json_normalize(json_dict['results']))
+            #df_ids = df_all[["author.id", "author.name"]]
+            #df_metrics = df_all[["metrics"]]
+            d = df_all[["author.id", "author.name", "metrics"]]
+            #print (pd.DataFrame([chain.from_iterable(i.values()) for i in d.values()],
+            #        index=d.keys(),
+            #        columns=[f"{k}_{num}" for k, v in list(d.values())[0].items()
+            #                 for num in range(len(v))]))
 
+            #df_m = pd.DataFrame()
+            #for d in df_metrics.values.tolist():
+            #    print(d)
+            #    print(pd.DataFrame(pd.json_normalize(d)))
+                #df_m = df_m.append(pd.DataFrame(pd.json_normalize(d)))
+                #df_ids.join(df_m)
+                
+                #df_m = df_m.join(pd.json_normalize(d, record_path=['values'], record_prefix='value-'))
+
+            #df_metrics = pd.DataFrame(pd.json_normalize(json.loads(df_all[["metrics"]].to_json(orient="records"))))
+            df_out = d
+            
+
+            if(i==0):
+                df_out.to_csv(outputfile, mode='w')
+            else:
+                df_out.to_csv(outputfile, mode='a', header=not os.path.exists(outputfile))
+            #pd.concat(df1, pd.json_normalize(json_dict['results']))
+            #print(pd.json_normalize(json_dict['results'][0]['author']['name']))
 
 
 if __name__ == "__main__":
