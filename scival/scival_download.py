3be16e3a995062550c158136a04f1f96d9a57922
Kiu Huang <ckhuang@life.hkbu.edu.hk>
2022-11-14 11:27:00 +0800

    tidy up data source url

--- scival/scival_download.py
+++ scival/scival_download.py
@@ -15,7 +15,11 @@ from flatten_json import flatten
 # max number of author ids
 max_per_request = 100
 
-# set the apiKey here
+# data source url
+url_datasource = 'https://api.elsevier.com/analytics/scival/author/metrics?'
+
+# set the apiKey here, the authors value will be read from input file
+# and seperate into requests if > max_per_request
 url_paras = {
     'apiKey': '7f59af901d2d86f78a1fd60c1bf9426a',
     'indexType': 'hIndex',
@@ -28,28 +32,23 @@ request_headers = {
     'Accept': 'application/json'
 }
 
-# search urls
-request_urls = {
-    'Academic_Corporate_Collaboration_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=AcademicCorporateCollaboration&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
-    'Collaboration_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=Collaboration&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
-    'Citations_Per_Publication_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=CitationsPerPublication&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
-    'Cited_Publications_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=CitedPublications&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
-    'Cited_Publications_all_years':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=CitationCount&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'Field_Weighted_Citation_Impact_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=FieldWeightedCitationImpact&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'HIndex_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=HIndices&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'HIndex_all_years':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=HIndices&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'ScholarlyOutput_2017-2022_by_year':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=ScholarlyOutput&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=true&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'ScholarlyOutput_all_years':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=ScholarlyOutput&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'Pubs_Top_Percentile_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=PublicationsInTopJournalPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-    'Outputs_Top_Percentile_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=OutputsInTopCitationPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true'
+# search request paras, can make it better using a json object
+request_paras = {
+    'Academic_Corporate_Collaboration_2017-2022':'metricTypes=AcademicCorporateCollaboration&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
+    'Collaboration_2017-2022':'metricTypes=Collaboration&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
+    'Citations_Per_Publication_2017-2022':'metricTypes=CitationsPerPublication&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
+    'Cited_Publications_2017-2022':'metricTypes=CitedPublications&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false',
+    'Cited_Publications_all_years':'metricTypes=CitationCount&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'Field_Weighted_Citation_Impact_2017-2022':'metricTypes=FieldWeightedCitationImpact&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'HIndex_2017-2022':'metricTypes=HIndices&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'HIndex_all_years':'metricTypes=HIndices&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'ScholarlyOutput_2017-2022_by_year':'metricTypes=ScholarlyOutput&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=true&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'ScholarlyOutput_all_years':'metricTypes=ScholarlyOutput&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'Pubs_Top_Percentile_2017-2022':'metricTypes=PublicationsInTopJournalPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
+    'Outputs_Top_Percentile_2017-2022':'metricTypes=OutputsInTopCitationPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true'
 }
 
-# testing for 1 url
-#request_urls = {
-#    'Pubs_Top_Percentile_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=PublicationsInTopJournalPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true',
-#    'Outputs_Top_Percentile_2017-2022':'https://api.elsevier.com/analytics/scival/author/metrics?metricTypes=OutputsInTopCitationPercentiles&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true'
-#}
-
+# main program
 def main(argv):
     inputfile = ''
     outputfile = ''
@@ -80,7 +79,7 @@ def main(argv):
         lines = f.read().splitlines()
 
     json_all = pd.DataFrame()
-    for myname, url_prefix in request_urls.items():
+    for myname, request_para in request_paras.items():
         json_metric = pd.DataFrame()
         
         for i in range(0, math.floor( len(lines) / max_per_request) + 1 ):
@@ -93,7 +92,7 @@ def main(argv):
             url_paras['authors'] = ','.join(lines[index_start:index_end])
             p = requests.models.PreparedRequest()
 
-            p.prepare_url(url=url_prefix, params=url_paras)
+            p.prepare_url(url=url_datasource + request_para, params=url_paras)
             #print(p.url)
             json_dict = json.loads(requests.get(p.url, headers=request_headers).text)
             json_results = pd.json_normalize(json_dict['results'])
@@ -109,7 +108,7 @@ def main(argv):
             json_all = json_metric
         else:
             json_all= json_all.merge(json_metric, how='left', on=['author.id', 'author.name'])
-        #print(json_all)
+
         sleep(randint(100,200)/100)
 
     #print(json_all)
